23be007542b8e45843b50c0b9e44f4fbbeca3f5a
7oltan <87986834+7oltan@users.noreply.github.com>
2024-01-04 16:38:33 +0300

    Update code.hx

--- code.hx
+++ code.hx
@@ -1,9 +1,10 @@
 function onCreate(){
     var SHIT = new FlxSprite(0,0);
     loadImageFromUrl(SHIT,'https://raw.githack.com/1Etoile1/eee/main/nastya/twige3.jpg',function(){
-        SHIT.setGraphicSize(400,400);
+       /* SHIT.setGraphicSize(400,400); 
         SHIT.updateHitBox();
-        SHIT.screenCenter();
+        SHIT.screenCenter();*/
+        trace('woah that worked!!');
     });
     gallery.add(SHIT);
 }
