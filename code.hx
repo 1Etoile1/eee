48b932d7a259e0f05e7819d36370a051dc019b81
7oltan <87986834+7oltan@users.noreply.github.com>
2024-01-04 16:49:54 +0300

    Update code.hx

--- code.hx
+++ code.hx
@@ -1,11 +1,17 @@
+var SHIT:FlxSprite;
+
+function afterLoad(){
+    SHIT.setGraphicSize(400,400); 
+    SHIT.updateHitBox();   
+    SHIT.screenCenter();
+    trace('woah that worked!!');
+}
+
 function onCreate(){
     trace('this shit actually traces... does it?');
-    var SHIT = new FlxSprite(0,0);
+    SHIT = new FlxSprite(0,0);
     loadImageFromUrl(SHIT,'https://raw.githack.com/1Etoile1/eee/main/nastya/twige3.jpg',function(){
-        /*SHIT.setGraphicSize(400,400); 
-        SHIT.updateHitBox();
-        SHIT.screenCenter();*/
-        trace('woah that worked!!');
+        this.executeFunction('afterLoad');
     });
     gallery.add(SHIT);
 }
