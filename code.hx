9b8327bc450eee991935aa2d04709e0804870e10
7oltan <87986834+7oltan@users.noreply.github.com>
2024-01-04 10:11:25 +0300

    Update code.hx

--- code.hx
+++ code.hx
@@ -1,6 +1,6 @@
 function onCreate(){
     var SHIT = new FlxSprite(0,0);
-    loadImageFromUrl(gallery.URL+'nastya/twige3.jpg');
+    loadImageFromUrl(SHIT,gallery.URL+'nastya/twige3.jpg');
     SHIT.screenCenter();
     gallery.add(SHIT);
 }
