3f738ac657da7e9806afc6e687f140b01d71b3f0
7oltan <87986834+7oltan@users.noreply.github.com>
2024-01-04 16:42:06 +0300

    Update code.hx

--- code.hx
+++ code.hx
@@ -1,7 +1,8 @@
 function onCreate(){
+    trace('this shit actually traces');
     var SHIT = new FlxSprite(0,0);
     loadImageFromUrl(SHIT,'https://raw.githack.com/1Etoile1/eee/main/nastya/twige3.jpg',function(){
-       /* SHIT.setGraphicSize(400,400); 
+        /*SHIT.setGraphicSize(400,400); 
         SHIT.updateHitBox();
         SHIT.screenCenter();*/
         trace('woah that worked!!');
