7693c1210973d9f7de7f20f6b0773cfd97a42604
7oltan <87986834+7oltan@users.noreply.github.com>
2024-01-04 16:46:16 +0300

    im actually sick of this help

--- code.hx
+++ code.hx
@@ -1,5 +1,5 @@
 function onCreate(){
-    trace('this shit actually traces');
+    trace('this shit actually traces... does it?');
     var SHIT = new FlxSprite(0,0);
     loadImageFromUrl(SHIT,'https://raw.githack.com/1Etoile1/eee/main/nastya/twige3.jpg',function(){
         /*SHIT.setGraphicSize(400,400); 
